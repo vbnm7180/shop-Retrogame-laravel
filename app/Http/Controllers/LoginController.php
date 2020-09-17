cb67b763c46a3ca01580539106b7d77af75961b3
vbnm7180 <vbnm7180@mail.ru>
2020-09-17 22:07:44 +0400

    master

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,9 +8,7 @@ class LoginController extends Controller
 {
     public function login(Request $request){
         $request->validate([
-            'login'=>'required|email|exists:users,login',
-            'password'=>'required|password'
-        ]);
+            'login'=>'required|email|exists:users,login',]);->after();
 
         return redirect('/');
 
