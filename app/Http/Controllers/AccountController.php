8ea4dc7f2785d4c5c65c7b633d3dfe2176edad2c
vbnm7180 <vbnm7180@mail.ru>
2020-09-20 00:30:09 +0400

    master

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,12 +7,50 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use App\Models\User;
 use App\Models\Order;
+use App\Models\GamesProduct;
+use App\Models\ConsolesProduct;
 
 class AccountController extends Controller
 {
     public function showAccount(){
 
-        $orders=Order::where('email','=',Auth::user()->email)->toArray();
+        $orders=Order::where('email','=',Auth::user()->email)->get()->toArray();
+
+        foreach ($orders as &$order){
+            //Преобразование перенчня товаров в массив
+            $ids = explode(', ', $order['products']);
+            foreach ($ids as $count=>$id){
+                	//Выбор id секции и id товара
+					$reg = '/\d+/';
+					preg_match_all($reg, $id, $arr);
+
+					$section_id = $arr[0][0];
+					$product_id = $arr[0][1];
+
+					//Выбор таблицы для запроса к бд и секции товара
+					if ($section_id == 1) {
+                        $product = ConsolesProduct::where('product_id', '=', $product_id)->get()->toArray();
+                        $products_info['product_name']=$product[0]['name'];
+                        $products_info['price']=$product[0]['price'];
+                        $products_info['section_name']='Игровая консоль';
+
+					}
+					if ($section_id == 2) {
+                        $product = GamesProduct::where('product_id', '=', $product_id)->get()->toArray();
+                        $products_info['product_name']=$product[0]['name'];
+                        $products_info['price']=$product[0]['price'];
+                        $products_info['section_name']='Игра';
+                    }
+                    $order['products_info'][$count]=$products_info;
+                    
+                    
+
+
+            }
+        }
+
+        unset($order);
+        //Log::info($orders);
 
         $res=[
             'city'=>Auth::user()->city,
@@ -23,11 +61,13 @@ class AccountController extends Controller
             'email'=>Auth::user()->email,
             'orders'=>$orders
          ];
+
+         Log::info($res);
          return view('account')->with('res',$res);
     }
 
     public function updateAccount(Request $input){
-        Log::info($input->all());
+        //Log::info($input->all());
        User::where('id','=',Auth::user()->id)->update($input->all());
 
     }
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,14 +1,14 @@
 @include ('header')
 
 <div class="btn-wraper">
-            <button type="button" class="logout-btn"><img src="/images/private_cab/logout.png" class="logout-btn" alt="logout"> Выйти</button>
-        </div>
+	<button type="button" class="logout-btn"><img src="/images/private_cab/logout.png" class="logout-btn" alt="logout"> Выйти</button>
+</div>
 <div class="pa__content">
 
 	<div class="pa__form-wrapper">
 
-	<form class="pa__form" id="account-form">
-		<label for="" class="form__title">личный кабинет</label>
+		<form class="pa__form" id="account-form">
+			<label for="" class="form__title">личный кабинет</label>
 			<input type="text" placeholder="ФИО" class="pa__input" name="name" value="{{$res['name']}}">
 			<input type="text" placeholder="Телефон" class="pa__input" name="phone" value="{{$res['phone']}}">
 			<input type="text" placeholder="Электронная почта" class="pa__input" name="email" value="{{$res['email']}}">
@@ -16,8 +16,8 @@
 			<input type="text" placeholder="Улица/Дом/Квартира" class="pa__input" name="street" value="{{$res['street']}}">
 			<input type="text" placeholder="Индекс" class="pa__input" name="postcode" value="{{$res['postcode']}}">
 			<div class="btn__wraper">
-			<button type="submit" class="personal__area-btn">Сохранить</button></div>
-	    </form>
+				<button type="submit" class="personal__area-btn">Сохранить</button></div>
+		</form>
 
 	</div>
 	<div class="form_separator"></div>
@@ -27,6 +27,41 @@
 			<div class="orders__title">Мои заказы <form action=""></form>
 			</div>
 
+			@foreach($res['orders'] as $order)
+
+			@php
+			//Вывод номера заказа и дополнение его нулями слева
+			$order_number = str_pad($order['order_number'], 7, "0", STR_PAD_LEFT);
+
+			//Вывод даты заказа
+			$date = date('d.m.Y', strtotime($order['order_date']));
+
+			$total = $order['total_price'];
+
+			//Счетчик для строки товара в заказе
+			$count = 1;
+			@endphp
+			<div class="orders__info">
+				<div class="orders__header">
+					<div class="order__number">№ {{$order_number}}</div>
+					<div class="order__date">{{$date}}</div>
+				</div>
+				<div class="orders__content">
+
+					@foreach($order['products_info'] as $product)
+
+					<div class="order">{{$count}}. {{$product['section_name']}} {{$product['product_name']}} <span>{{$product['price']}} р</span> </div>
+
+					@php
+					$count++;
+					@endphp
+					@endforeach
+
+					<div class="orders__price">
+						Общая стоимость: <span>{{$total}} р</span></div>
+				</div>
+			</div>
+			@endforeach
 		</div>
 	</div>
 </div>
